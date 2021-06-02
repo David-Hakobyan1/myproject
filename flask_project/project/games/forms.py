2c6a14a53eafea2c53f5b862fef44b8d2fc00602
David-Hakobyan1 <84774130+David-Hakobyan1@users.noreply.github.com>
2021-06-02 22:41:43 +0300

    Delete flask_project directory

--- flask_project/project/games/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
-
-class PLAY_Form(FlaskForm):
-    fnum = StringField("Your Answer:",validators = [DataRequired()])
-    play = SubmitField("Ok")
-
