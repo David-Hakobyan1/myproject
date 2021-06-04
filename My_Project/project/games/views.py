6adcd66e09fe22145c5dbf867181e7b165f4d53e
David Hakobyan <my@mail.ru>
2021-06-04 13:12:18 +0300

    update game

--- My_Project/project/games/views.py
+++ My_Project/project/games/views.py
@@ -2,6 +2,6 @@ from flask import render_template,Blueprint
 
 games = Blueprint('games',__name__)
 
-@games.route("/gams",methods=['GET','POST'])
+@games.route("/my_game",methods=['GET','POST'])
 def game():
     return render_template("games.html")
